6e7a8755421eb111a6d416176cbc811fac4c73a1
Rani1234m <rani.adepu4@gmail.com>
2026-02-01 23:00:52 +1100

    Add files via upload
    day 1 assignment

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String name ="John Doe";
+    byte age =30;
+    float salary =55000.75f;
+    char gender='M';
+    boolean acceptTerms=true;
+    float rating=4.5f;
+    long mobileNumber=9876543210L;
+    int pincode=56001;
+    char luckyNumber='7';
+    System.out.println(name+"\n"+age+"\n"+salary+"\n"+gender+"\n"+acceptTerms+"\n"+rating+"\n"+mobileNumber+"\n"+pincode+"\n"+luckyNumber);
+    
+    
+    
+    		
+    
+	}
+
+}
